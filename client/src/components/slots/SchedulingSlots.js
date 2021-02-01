1ca424edf7c32ea0e1382df6f9c2f4c3b696f44f
Sandra Spighel <s.spighel@gmail.com>
2021-02-01 15:10:16 +0000

    FRONT-SCHEDULE-Calendar listing all events with film title or Free slot

--- client/src/components/slots/SchedulingSlots.js
+++ client/src/components/slots/SchedulingSlots.js
@@ -1,77 +1,60 @@
 import React from 'react'
 import { getAllSlots } from '../lib/api'
-
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
 import 'react-big-calendar/lib/sass/styles.scss'
-// import 'react-big-calendar/lib/addons/dragAndDrop/withDragAndDrop'
 
 function SchedulingSlots(){
 
+  const localizer = momentLocalizer(moment)
   const [slots, setSlots] = React.useState(null)
-  const [hasErr, setHasErr] = React.useState(false)
+  // const [hasErr, setHasErr] = React.useState(false)
 
-  // * Gets the array of scheduling slots objects 
+  // * Gets the array of scheduling slots objects
   React.useEffect(() => {
     const getData = async () => {
       try {
         const { data } = await getAllSlots()
-        setSlots(data)
 
+        const sortedData = data.sort((a, b) => Date.parse(a.startTime) - Date.parse(b.startTime))
+        console.log('sorted data *** ', sortedData)
+
+        setSlots(data)
       } catch (err) {
-        setHasErr(err)
+        console.log(err)
       }
     }
     getData()
   }, [])
 
-  console.log('slots are: ', slots)
-  console.log('has error: ', hasErr)
+  const events = slots && slots.map(slot => { // * if the slots are null, this will just be false, but will format the events correctly where the slots are here
 
-  const localizer = momentLocalizer(moment)
+    return {
+      id: slot.id,
+      title: !slot.film ? 'Free slot' : slot.film.title,
+      allDay: false,
+      start: new Date(slot.startTime),
+      end: new Date(slot.endTime),
+      resource: slot.cinema.name
+    }
+  })
 
-  // * Creates new object setting slots in format required to be displayed in the calendar
-  if (slots) {
-    console.log(slots.title)
-    const formattedSlots = slots.map(slot => {
-      return {
-        id: slot.id,
-        title: slot.film,
-        allDay: false,
-        start: new Date(slot.startTime),
-        end: new Date(slot.endTime),
-        resource: slot.cinema.name
-      }
-    })
-
-    console.log('formattedSlots' ,formattedSlots)
-  }
-  
-
-  // The events object which displays events on the calendar
-  const events = [{
-    id: 0,
-    title: 'No slot allocated',
-    allDay: false,
-    start: new Date(2021, 6, 17, 18, 0, 0),
-    end: new Date(2021, 6, 17, 20, 30, 0)
-  }]
-    
-
+  console.log('formatted slots are: ', events)
   return (
     <div className="container">
-
-      <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        defaultDate={new Date(2021, 6, 17)}
-        defaultView="week"
-        style={{ height: '100vh' }}
-      />
+      {slots ? // * only render the calender if slots exist
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          defaultDate={new Date(2021, 5, 14)}
+          defaultView="week"
+          style={{ height: '100vh' }}
+        />
+        : <p>..loading</p>
+      }
     </div>
   )
 }
-
 export default SchedulingSlots
